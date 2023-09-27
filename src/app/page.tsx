7bf88e7d255a3560ede02365a723368ca0cdf2d7
inacio-dev <inaciormgalvao@gmail.com>
2023-09-26 21:27:37 -0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen w-full items-center justify-center text-3xl text-slate-dark-1">
-      <h1>Next Template Teams</h1>
+      <h1>Next Template Teams 2</h1>
     </main>
   )
 }
